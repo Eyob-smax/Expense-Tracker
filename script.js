0fd091d340e4e89de7ebf0d2f6e5e9e82f97bf07
Eyoba_Smax <eyobsmax@gmail.com>
2025-02-14 23:34:37 +0300

    UI element styling finishes

---
+++ script.js
@@ -0,0 +1,3 @@
+const form = document.querySelector(".form-container");
+const inputsContainer = document.querySelector(".inputs-container");
+const chartContainer = document.querySelector(".expenses-container");
